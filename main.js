1d17291a2af5ba24f45e7dbcfc54bde76e3e5bb2
Alexander Gonzalez <julian1alex@gmail.com>
2019-10-20 19:17:00 -0700

    added

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$(document).ready(function (){
+    $(document).ready(function (){
      $.get("https://obscure-tundra-54269.herokuapp.com/fine-dining")
     .done(function(data){
         let apps = data.appetizers
